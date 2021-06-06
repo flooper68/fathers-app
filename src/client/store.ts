3306a9aaf7bbc4fcd3c27f06ef982ab78c271111
flooper68 <premysl.ciompa@gmail.com>
2021-06-06 18:10:59 +0200

    Setting up redux store and client sync implementation

---
+++ src/client/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+import { syncReducer } from './root/sync'
+
+export const store = configureStore({
+  reducer: {
+    sync: syncReducer,
+  },
+})
+
+export type StoreState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<StoreState> = useSelector
