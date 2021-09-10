7450195603c0a9b41c6b08080e61e4a0ea309529
Peta <PetrHlinka@email.cz>
2021-09-10 08:25:14 +0200

    updating green coffee

--- src/server/roasting/repositories/roasted-coffee-product-repository.ts
+++ src/server/roasting/repositories/roasted-coffee-product-repository.ts
@@ -1,17 +1,22 @@
 import { FindOneRoastedCoffeeProduct } from './../interfaces/roasting-repository';
 
 const RoastedCoffeeProductMap: Record<number, number> = {
-  [13277]: 1, // Kolumbie - La Colombia Filtr, Káva
-  [13286]: 2, // Kolumbie - La Colombia Espresso, Káva
-  [9318]: 3, // Peru - El Paraiso Filtr, Káva
-  [9331]: 4, // Peru - El Paraiso Filtr, Káva
-  [4135]: 5, // Brazílie - PAUBRASIL Espresso
-  [13234]: 6, //Kolumbie - Canas Gordas Filtr
-  [13268]: 7, // Kolumbie - Canas Gordas Espresso
-  [12955]: 8, // Brazilie - Manga larga Filtr
-  [12930]: 9, // Brazilie - Manga larga Espresso
-  [12994]: 10, // Brazilie - Cafeina Espresso
-  [13477]: 11, // Kenya - Gititu AA Filtr
+  [12994]: 1, // Brazilie - Cafeina Espresso
+  [12955]: 2, // Brazilie - Manga larga Filtr
+  [12930]: 3, // Brazilie - Manga larga Espresso
+  [13234]: 4, // Kolumbie - Canas Gordas Filtr
+  [13268]: 5, // Kolumbie - Canas Gordas Espresso
+  [13477]: 6, // Kenya - Gititu AA Filtr
+  [14224]: 7, // Nikaragua - Bethania washed Filtr
+  [14216]: 8, // Nikaragua - Bethania natural Filtr
+  [14201]: 9, // Nikaragua - Los Nubarrones Filtr
+  [14178]: 10, // Nikaragua - Los Nubarrones Espresso
+  [14426]: 11, // Indonésie - Pegasing washed Filtr
+  [14473]: 12, // Indonésie - Pegasing washed Espresso
+  [14481]: 13, // Indonésie - Pegasing natural Filtr
+  [14447]: 14, // Indonésie - Pegasing natural Espresso
+  [14496]: 15, // Indonésie - Pegasing anaerobic Filtr
+  [14633]: 16, // Nikaragua - La Coquimba Espresso
 };
 
 export const findOneRoastedCoffeeProduct: FindOneRoastedCoffeeProduct = async (conditions: {
--- src/server/roasting/repositories/roasted-coffee-repository.ts
+++ src/server/roasting/repositories/roasted-coffee-repository.ts
@@ -5,17 +5,42 @@ import {
 } from './../interfaces/roasting-repository';
 
 const RoastedCoffeeMap: Record<number, RoastedCoffee> = {
-  [1]: { id: 1, name: 'Kolumbie - La Colombia Filtr', greenCoffeeId: 1 },
-  [2]: { id: 2, name: 'Kolumbie - La Colombia Espresso', greenCoffeeId: 1 },
-  [3]: { id: 3, name: 'Peru - El Paraiso Filtr', greenCoffeeId: 2 },
-  [4]: { id: 4, name: 'Peru - El Paraiso Espresso', greenCoffeeId: 2 },
-  [5]: { id: 5, name: 'Brazílie - PAUBRASIL Espresso', greenCoffeeId: 3 },
-  [6]: { id: 6, name: 'Kolumbie - Canas Gordas Filtr', greenCoffeeId: 4 },
-  [7]: { id: 7, name: 'Kolumbie - Canas Gordas Espresso', greenCoffeeId: 4 },
-  [8]: { id: 8, name: 'Brazilie - Manga larga Filtr', greenCoffeeId: 5 },
-  [9]: { id: 9, name: 'Brazilie - Manga larga Espresso', greenCoffeeId: 5 },
-  [10]: { id: 10, name: 'Brazilie - Cafeina Espresso', greenCoffeeId: 6 },
-  [11]: { id: 11, name: 'Kenya - Gititu AA Filtr', greenCoffeeId: 7 },
+  [1]: { id: 1, name: 'Brazilie - Cafeina Espresso', greenCoffeeId: 1 },
+  [2]: { id: 2, name: 'Brazilie - Manga larga Filtr', greenCoffeeId: 2 },
+  [3]: { id: 3, name: 'Brazilie - Manga larga Espresso', greenCoffeeId: 2 },
+  [4]: { id: 4, name: 'Kolumbie - Canas Gordas Filtr', greenCoffeeId: 3 },
+  [5]: { id: 5, name: 'Kolumbie - Canas Gordas Espresso', greenCoffeeId: 3 },
+  [6]: { id: 6, name: 'Kenya - Gititu AA Filtr', greenCoffeeId: 4 },
+  [7]: { id: 7, name: 'Nikaragua - Bethania washed Filtr', greenCoffeeId: 5 },
+  [8]: { id: 8, name: 'Nikaragua - Bethania natural Filtr', greenCoffeeId: 6 },
+  [9]: { id: 9, name: 'Nikaragua - Los Nubarrones Filtr', greenCoffeeId: 7 },
+  [10]: {
+    id: 10,
+    name: 'Nikaragua - Los Nubarrones Espresso',
+    greenCoffeeId: 7,
+  },
+  [11]: { id: 11, name: 'Indonésie - Pegasing washed Filtr', greenCoffeeId: 8 },
+  [12]: {
+    id: 12,
+    name: 'Indonésie - Pegasing washed Espresso',
+    greenCoffeeId: 8,
+  },
+  [13]: {
+    id: 13,
+    name: 'Indonésie - Pegasing natural Filtr',
+    greenCoffeeId: 9,
+  },
+  [14]: {
+    id: 14,
+    name: 'Indonésie - Pegasing natural Espresso',
+    greenCoffeeId: 9,
+  },
+  [15]: {
+    id: 15,
+    name: 'Indonésie - Pegasing anaerobic Filtr',
+    greenCoffeeId: 10,
+  },
+  [16]: { id: 16, name: 'Nikaragua - La Coquimba Espresso', greenCoffeeId: 11 },
 };
 
 export const findRoastedCoffee: FindRoastedCoffee = async (conditions) => {
