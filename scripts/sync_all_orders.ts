ea4fa26aeeb4cdfd9592926da94204cb192e250a
flooper68 <premysl.ciompa@gmail.com>
2021-09-22 11:25:20 +0200

    fix scripts

--- scripts/sync_all_orders.ts
+++ scripts/sync_all_orders.ts
@@ -1,17 +1,59 @@
-import mongoose from 'mongoose'
-import { config } from 'dotenv'
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
 
-import {
-  mapOrder,
-  syncOrder,
-} from '../src/server/services/data-sync/sync-orders'
-import { buildWooCommerceClient } from '../src/server/services/woocommerce'
-import { Logger } from '../src/shared/logger'
+import { Logger } from '../src/shared/logger';
+import { OrderModel } from '../src/server/sales/repository/order-model';
+import { buildWooCommerceClient } from '../src/server/services/woocommerce-client';
+import { WooCommerceOrderResponse, Order } from '../src/shared/types/order';
 
-config()
+config();
 
-const start = Date.now()
-Logger.info(`Started syncing all orders`)
+const mapOrder = (order: WooCommerceOrderResponse): Order => {
+  return {
+    id: order.id,
+    number: order.number,
+    dateCreated: order.date_created,
+    dateModified: order.date_modified,
+    status: order.status,
+    lineItems: order.line_items.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        productName: item.parent_name,
+        productId: item.product_id,
+        variationId: item.variation_id,
+        quantity: item.quantity,
+      };
+    }),
+    roasted: false,
+  };
+};
+
+const syncOrder = async (order: Order) => {
+  const dbEntity = await OrderModel.findOne({
+    id: order.id,
+  }).exec();
+
+  if (dbEntity) {
+    Logger.debug(`Order ${order.id} exists`);
+
+    if (dbEntity.dateModified !== order.dateModified) {
+      Logger.info(`Order ${order.id} was modified, updating`);
+      await dbEntity.updateOne(order);
+      return;
+    } else {
+      Logger.debug(`Order ${order.id} was not modified`);
+      return;
+    }
+  } else {
+    Logger.info(`Order ${order.id} does not exist, creating new entry`);
+    const dbEntity = new OrderModel(order);
+    await dbEntity.save();
+  }
+};
+
+const start = Date.now();
+Logger.info(`Started syncing all orders`);
 
 mongoose
   .connect(
@@ -22,47 +64,47 @@ mongoose
     }
   )
   .then(async () => {
-    Logger.info(`Connected to db`)
+    Logger.info(`Connected to db`);
 
-    const woocommerceClient = await buildWooCommerceClient()
+    const woocommerceClient = await buildWooCommerceClient();
 
     try {
-      let page = 1
+      let page = 1;
 
       let results = await woocommerceClient.getOrders({
         orderby: 'date',
         order: 'asc',
-      })
+      });
 
       while (results.rows.length > 0) {
-        Logger.info(`Fetched ${results.rows.length} results`)
+        Logger.info(`Fetched ${results.rows.length} results`);
 
-        const lastOrder = mapOrder(results.rows[results.rows.length - 1])
+        const lastOrder = mapOrder(results.rows[results.rows.length - 1]);
         await Promise.all(
           results.rows.map(mapOrder).map((order) => syncOrder(order))
-        )
+        );
         Logger.info(
           `Fetching page ${page}, running for ${(Date.now() - start) / 1000} s`
-        )
+        );
 
-        page++
+        page++;
 
         results = await woocommerceClient.getOrders({
           after: lastOrder.dateCreated,
           orderby: 'date',
           order: 'asc',
-        })
+        });
       }
     } catch (e) {
-      Logger.error(`Error syncing orders`, e)
+      Logger.error(`Error syncing orders`, e);
     }
 
-    Logger.info(`No more results, syncing done`)
+    Logger.info(`No more results, syncing done`);
 
-    await mongoose.disconnect()
-    Logger.info(`Sync finished  ${(Date.now() - start) / 1000} s`)
+    await mongoose.disconnect();
+    Logger.info(`Sync finished  ${(Date.now() - start) / 1000} s`);
   })
 
   .catch((e) => {
-    Logger.error('Error connecting to db', e)
-  })
+    Logger.error('Error connecting to db', e);
+  });
--- scripts/sync_some_orders.ts
+++ scripts/sync_some_orders.ts
@@ -1,18 +1,60 @@
-import mongoose from 'mongoose'
-import { config } from 'dotenv'
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+import moment from 'moment';
 
-import {
-  mapOrder,
-  syncOrder,
-} from '../src/server/services/data-sync/sync-orders'
-import { buildWooCommerceClient } from '../src/server/services/woocommerce'
-import { Logger } from '../src/shared/logger'
-import moment from 'moment'
+import { Logger } from '../src/shared/logger';
+import { buildWooCommerceClient } from '../src/server/services/woocommerce-client';
+import { OrderModel } from '../src/server/sales/repository/order-model';
+import { WooCommerceOrderResponse, Order } from '../src/shared/types/order';
 
-config()
+config();
 
-const start = Date.now()
-Logger.info(`Started syncing some data`)
+const start = Date.now();
+Logger.info(`Started syncing some data`);
+
+const mapOrder = (order: WooCommerceOrderResponse): Order => {
+  return {
+    id: order.id,
+    number: order.number,
+    dateCreated: order.date_created,
+    dateModified: order.date_modified,
+    status: order.status,
+    lineItems: order.line_items.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        productName: item.parent_name,
+        productId: item.product_id,
+        variationId: item.variation_id,
+        quantity: item.quantity,
+      };
+    }),
+    roasted: false,
+  };
+};
+
+const syncOrder = async (order: Order) => {
+  const dbEntity = await OrderModel.findOne({
+    id: order.id,
+  }).exec();
+
+  if (dbEntity) {
+    Logger.debug(`Order ${order.id} exists`);
+
+    if (dbEntity.dateModified !== order.dateModified) {
+      Logger.info(`Order ${order.id} was modified, updating`);
+      await dbEntity.updateOne(order);
+      return;
+    } else {
+      Logger.debug(`Order ${order.id} was not modified`);
+      return;
+    }
+  } else {
+    Logger.info(`Order ${order.id} does not exist, creating new entry`);
+    const dbEntity = new OrderModel(order);
+    await dbEntity.save();
+  }
+};
 
 mongoose
   .connect(
@@ -23,30 +65,30 @@ mongoose
     }
   )
   .then(async () => {
-    Logger.info(`Connected to db`)
+    Logger.info(`Connected to db`);
 
-    const woocommerceClient = await buildWooCommerceClient()
+    const woocommerceClient = await buildWooCommerceClient();
 
     try {
       const results = await woocommerceClient.getOrders({
         before: moment().subtract(1, 'days').toISOString(),
-      })
+      });
 
-      Logger.info(`Fetched ${results.rows.length} results`)
+      Logger.info(`Fetched ${results.rows.length} results`);
 
       await Promise.all(
         results.rows.map(mapOrder).map((order) => syncOrder(order))
-      )
+      );
     } catch (e) {
-      Logger.error(`Error syncing orders`, e)
+      Logger.error(`Error syncing orders`, e);
     }
 
-    Logger.info(`No more results, syncing done`)
+    Logger.info(`No more results, syncing done`);
 
-    await mongoose.disconnect()
-    Logger.info(`Sync finished  ${(Date.now() - start) / 1000} s`)
+    await mongoose.disconnect();
+    Logger.info(`Sync finished  ${(Date.now() - start) / 1000} s`);
   })
 
   .catch((e) => {
-    Logger.error('Error connecting to db', e)
-  })
+    Logger.error('Error connecting to db', e);
+  });
