bfd1524023dd5ac7e4f6eb839ac7e8f1c84d9806
flooper68 <premysl.ciompa@gmail.com>
2021-10-11 09:29:37 +0200

    Production docker-compose added

--- migrations/index.ts
+++ migrations/index.ts
@@ -5,7 +5,8 @@ import { config } from 'dotenv';
 config();
 
 mongoMigrateCli({
-  uri: `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_DATABASE_NAME}?authSource=${process.env.MONGO_DB_AUTHENTICATION_DATABASE_NAME}`,
+  uri: `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}`,
+  database: 'fathers',
   migrationsDir: __dirname,
   migrationsCollection: 'migrations_collection',
   options: {
